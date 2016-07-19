06215a560f116ee313b91277863648ee6a818d1e
Sami <sami.zara91@gmail.com>
2016-07-19 11:22:50 +0300

    Asteroid repeller added

---
+++ Assets/Scripts/Game/Obstacles/ObstacleRepeller.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObstacleRepeller : MonoBehaviour
+{
+
+    public List<GameObject> asteroidsInRange = new List<GameObject>();
+    public float repelForce;
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(Repeller());
+    }
+
+    /*void Update()
+    {
+        foreach (GameObject asteroid in asteroidsInRange)
+        {
+            if (asteroid != null)
+            {
+                float degree = MathHelper.degreeBetween2Points(transform.position, asteroid.transform.position);
+                asteroid.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(degree * Mathf.Deg2Rad) * repelForce, Mathf.Sin(degree * Mathf.Deg2Rad) * repelForce));
+            }
+            else
+                asteroidsInRange.Remove(asteroid);
+        }
+    }*/
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        asteroidsInRange.Add(collider.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        asteroidsInRange.Remove(collider.gameObject);
+    }
+
+    IEnumerator Repeller()
+    {
+        while (true)
+        {
+            foreach (GameObject asteroid in asteroidsInRange)
+            {
+                if (asteroid != null)
+                {
+                    float degree = MathHelper.degreeBetween2Points(transform.position, asteroid.transform.position);
+                    asteroid.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(degree * Mathf.Deg2Rad) * repelForce, Mathf.Sin(degree * Mathf.Deg2Rad) * repelForce));
+                }
+                else
+                    asteroidsInRange.Remove(asteroid);
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
