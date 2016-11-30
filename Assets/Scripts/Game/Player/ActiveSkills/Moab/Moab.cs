43e1937af0ef6f811cf0472b53484d951739612a
Sami <sami.zara91@gmail.com>
2016-11-30 10:22:56 +0200

    Bug fix

--- Assets/Scripts/Game/Player/ActiveSkills/Moab/Moab.cs
+++ Assets/Scripts/Game/Player/ActiveSkills/Moab/Moab.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class Moab : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
         Instantiate(ResourceManager.Instance.AllocateAndDump("Prefabs/ActiveSkillsProjectiles/MOAB/MoabProjectile"), transform.position+new Vector3(0,0,-1), transform.parent.rotation);
+        gameObject.SetActive(false);
     }
 	
 	// Update is called once per frame
